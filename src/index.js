eb73074488018ae7ed6ac480a1d32492e7552dba
hoanganhminh <hoanganhminh120601@gmail.com>
2024-03-22 17:54:10 +0700

    change index, middleware and delete pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 import reducer from './reducers';
 import middleware from './middleware';
 import App from './App';
--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 import { applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 const middleware = applyMiddleware(thunk);
 
